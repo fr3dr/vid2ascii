25255849446682b775dc48d2e4fc5f7bb2d49f12
fr3dr <ludaut@outlook.com>
2023-05-04 14:14:46 -0700

    Put all the logic into functions

--- main.py
+++ main.py
@@ -8,10 +8,6 @@ INPUT_DIR = "input"
 OUTPUT_DIR = "output"
 
 
-def ger_char(input):
-    return chars_list[int(input*interval)]
-
-
 input_vid = "input.mp4"
 fps = 30
 
@@ -25,52 +21,71 @@ scale = 0.2
 char_width = 6
 char_height = char_width*2
 
-if os.path.exists(INPUT_DIR):
-    shutil.rmtree(INPUT_DIR)
-if os.path.exists(OUTPUT_DIR):
-    shutil.rmtree(OUTPUT_DIR)
-os.mkdir(INPUT_DIR)
-os.mkdir(OUTPUT_DIR)
 
-print("Converting to images")
-subprocess.run(f"ffmpeg -i \"{input_vid}\" -q:a 0 -map a audio.mp3 -r {fps} input/img_%1d.jpg", shell=True)
+def ger_char(input):
+    return chars_list[int(input*interval)]
 
-# TODO: estimate the time it takes converting to ascii using this
-# get number of files in input directory
-file_count = 0
-for entry in os.listdir(INPUT_DIR):
-    file_count += 1
 
-# convert each image in input directory to ascii
-index = 1
-for file in os.listdir(INPUT_DIR):
-    print(f"Converting to ascii... {index}/{file_count}")
+def convert_img():
+    if os.path.exists(INPUT_DIR):
+        shutil.rmtree(INPUT_DIR)
+    if os.path.exists(OUTPUT_DIR):
+        shutil.rmtree(OUTPUT_DIR)
+    os.mkdir(INPUT_DIR)
+    os.mkdir(OUTPUT_DIR)
 
-    # open input image
-    im = Image.open(f"input/img_{index}.jpg").convert("RGB")
+    print("Converting to images")
+    subprocess.run(f"ffmpeg -i \"{input_vid}\" -q:a 0 -map a audio.mp3 -r {fps} input/img_%1d.jpg", shell=True)
 
-    # resize input image
-    width, height = (1080, 720)
-    im = im.resize((int(scale*width), int(scale*height*(char_width/char_height))), Image.NEAREST)
-    width, height = im.size
 
-    pix = im.load()
+def convert_vid():
+    print("Converting to video")
+    subprocess.run(f"ffmpeg -i audio.mp3 -r {fps} -i output/img_%1d.jpg output.mp4", shell=True)
+    print("DONE! (hopefully)")
 
-    # create output image
-    output_image = Image.new("RGB", (char_width*width, char_height*height), color=(0, 0, 0))
-    draw = ImageDraw.Draw(output_image)
 
-    # Loop through each pixel in the input image and calculate intensity
-    for y in range(height):
-        for x in range(width):
-            r, g, b = pix[x, y]
-            intensity = int(r/3 + g/3 + b/3)
-            draw.text((x*char_width, y*char_height), ger_char(intensity), (r, g, b))
+def get_file_count():
+    # TODO: estimate the time it takes converting to ascii using this
+    # get number of files in input directory
+    file_count = 0
+    for entry in os.listdir(INPUT_DIR):
+        file_count += 1
+    return file_count
 
-    output_image.save(f"output/img_{index}.jpg")
 
-    index += 1
+def asciify(file_count):
+    # convert each image in input directory to ascii
+    index = 1
+    for file in os.listdir(INPUT_DIR):
+        print(f"Converting to ascii... {index}/{file_count}")
 
-print("Converting to video")
-subprocess.run(f"ffmpeg -i audio.mp3 -r {fps} -i output/img_%1d.jpg output.mp4", shell=True)
-print("DONE! (hopefully)")
+        # open input image
+        im = Image.open(f"input/img_{index}.jpg").convert("RGB")
+
+        # resize input image
+        width, height = (1080, 720)
+        im = im.resize((int(scale*width), int(scale*height*(char_width/char_height))), Image.NEAREST)
+        width, height = im.size
+
+        pix = im.load()
+
+        # create output image
+        output_image = Image.new("RGB", (char_width*width, char_height*height), color=(0, 0, 0))
+        draw = ImageDraw.Draw(output_image)
+
+        # Loop through each pixel in the input image and calculate intensity
+        for y in range(height):
+            for x in range(width):
+                r, g, b = pix[x, y]
+                intensity = int(r/3 + g/3 + b/3)
+                draw.text((x*char_width, y*char_height), ger_char(intensity), (r, g, b))
+
+        output_image.save(f"output/img_{index}.jpg")
+
+        index += 1
+
+
+if __name__ == "__main__":
+    convert_img()
+    asciify(get_file_count())
+    convert_vid()
